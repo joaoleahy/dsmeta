5e045d5d100d7ab91e197e5cea7c391607c79c64
João Leahy <jvm.leahy@gmail.com>
2022-09-10 09:12:12 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
-import NotificationButton from "./components/NotificationButton"
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-  return(
+  return (
     <>
-    <Header />
-    <main>
-      <section id="sales">
-        <div className="dsmeta-container">
-          <SalesCard />
-        </div>
+      <ToastContainer />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
         </section>
       </main>
-    </>
-  )
+    </>)
 }
 
-export default App
+export default App;
